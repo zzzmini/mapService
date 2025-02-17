0bc1b365497102f091a94e39fd1abf1de9b987bf
zzzmini <sm01079200887@gmail.com>
2025-02-17 11:31:30 +0900

    #5-Session처리(Base)

---
+++ src/main/java/com/my/mapService/controller/SessionController.java
@@ -0,0 +1,44 @@
+package com.my.mapService.controller;
+
+import com.my.mapService.dto.User;
+import com.my.mapService.service.UserServiceImpl;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
+
+@Controller
+public class SessionController {
+    private final UserServiceImpl userService;
+
+    public SessionController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/login")
+    public String loginProcess(User user, HttpSession session) {
+        System.out.println("login Info : " + user);
+        Optional<User> sessionUser = userService
+                .findById(user.getUserId());
+        // 해당 회원정보를 찾았을 경우, 못 찾았을 경우(null)
+        // 스프링에서 널값 또는 빈값을 비교할 때 사용...
+        if (! ObjectUtils.isEmpty(sessionUser)) {
+            if (user.getPassword().equals(
+                sessionUser.get().getPassword())) {
+                // 로그인 성공
+                // 세션을 생성해서 전달
+                // 비밀번호 확인 후
+                session.setAttribute("sessionInfo", sessionUser);
+                return "home";
+            } else {
+                // 로그인 실패
+                return "/users/login";
+            }
+        } else {
+            // 로그인 실패
+            return "/users/login";
+        }
+    }
+}
